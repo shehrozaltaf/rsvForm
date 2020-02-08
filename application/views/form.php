c08d3c9a99865c278768ff818eec17620f316032
shahroz.khan <shehroz.altaf92@gmail.com>
2020-02-08 18:14:21 +0500

    Forms

--- application/views/form.php
+++ application/views/form.php
@@ -42,17 +42,76 @@
             <div class="md-card-content">
                 <h3 class="heading_a">Eosinophil count test</h3>
                 <div class="uk-grid" data-uk-grid-margin>
+                    <div class="uk-width-medium-1-1">
+                        <div class="uk-form-row">
+                            <div class="uk-grid" data-uk-grid-margin>
+                                <div class="uk-width-medium-1-3">
+                                    <label for="dssid">DSS ID</label>
+                                    <input type="text" class="md-input label-fixed" id="dssid" name="dssid" disabled
+                                           readonly/>
+                                </div>
+                                <div class="uk-width-medium-1-3">
+                                    <label for="mother_name">Mother Name</label>
+                                    <input type="text" class="md-input label-fixed" id="mother_name" name="mother_name"
+                                           disabled readonly/>
+                                </div>
+                                <div class="uk-width-medium-1-3">
+                                    <label for="father_name">Father Name</label>
+                                    <input type="text" class="md-input label-fixed" id="father_name" name="father_name"
+                                           disabled
+                                           readonly/>
+                                </div>
+                                <!--<div class="uk-width-medium-1-3">
+                                    <label for="areacode">Area Code</label>
+                                    <input type="text" class="md-input label-fixed" id="areacode" name="areacode"
+                                           disabled
+                                           readonly/>
+                                </div>
 
+
+                                <div class="uk-width-medium-1-3">
+                                    <label for="hhhead">Household Head</label>
+                                    <input type="text" class="md-input label-fixed" id="hhhead" name="hhhead" disabled
+                                           readonly/>
+                                </div>
+                                <div class="uk-width-medium-1-3">
+                                    <label for="dob">DOB</label>
+                                    <input type="text" class="md-input label-fixed" id="dob" name="dob" disabled
+                                           readonly/>
+                                </div>
+                                <div class="uk-width-medium-1-3">
+                                    <label for="study_id">Study ID</label>
+                                    <input type="text" class="md-input label-fixed" id="study_id" name="study_id"
+                                           disabled
+                                           readonly/>
+                                </div>
+                                <div class="uk-width-medium-1-3">
+                                    <label for="gender">Gender</label>
+                                    <input type="text" class="md-input label-fixed" id="gender" name="gender" disabled
+                                           readonly/>
+                                </div>
+                                <div class="uk-width-medium-1-3">
+                                    <label for="child_name">Child Name</label>
+                                    <input type="text" class="md-input label-fixed" id="child_name" name="child_name"
+                                           disabled readonly/>
+                                </div>-->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="uk-grid" data-uk-grid-margin>
                     <div class="uk-width-medium-1-1">
                         <div class="uk-form-row">
                             <div class="uk-grid" data-uk-grid-margin>
                                 <div class="uk-width-medium-1-3">
                                     <label for=" ">Date when blood sample was collected at PHC</label>
-                                    <input class=" datePicker" id="participantID" required name="participantID"/>
+                                    <input class=" datePicker" id="participantID" value="<?php echo date('d/m/Y') ?>"
+                                           required name="participantID"/>
                                 </div>
                                 <div class="uk-width-medium-1-3">
                                     <label for=" ">Date when blood sample was received by lab</label>
-                                    <input class=" datePicker" id="participantID" required name="participantID"/>
+                                    <input class=" datePicker" id="participantID" value="<?php echo date('d/m/Y') ?>"
+                                           required name="participantID"/>
                                 </div>
                                 <div class="uk-width-medium-1-3">
                                     <label for=" ">Condition of sample when received by lab</label>
@@ -153,26 +212,24 @@
                         </div>
                     </div>
                 </div>
+                <input type="button" value="Send All" id="btn-Edit" onclick="addData()" class="md-btn md-btn-primary">
             </div>
         </div>
     </div>
 </div>
-<!-- page specific plugins -->
-<!-- kendo UI -->
-
-<!-- additional styles for plugins -->
-<!-- kendo UI -->
 <link rel="stylesheet"
       href="<?php echo base_url() ?>assets/bower_components/kendo-ui/styles/kendo.common-material.min.css"/>
 <link rel="stylesheet" href="<?php echo base_url() ?>assets/bower_components/kendo-ui/styles/kendo.material.min.css"
       id="kendoCSS"/>
-
 <script src="<?php echo base_url() ?>assets/js/kendoui_custom.min.js"></script>
 
-<!--  kendoui functions -->
-<!--<script src="--><?php //echo base_url() ?><!--assets/js/pages/kendoui.min.js"></script>-->
-
 <script>
+    $(document).ready(function () {
+        $('.datePicker').kendoDatePicker({
+            format: "dd-MM-yyyy"
+        });
+    });
+
     $("#srchDssID").keyup(function () {
         var srchDss = $('#srchDssID').val();
         if (srchDss.length >= 4) {
@@ -205,6 +262,15 @@
                                 "<span class='md-list-heading'>" + v.dssid + "</span>" +
                                 "<span class='uk-text-small uk-text-muted'>Mother: " + v.mother_name + "</span>" +
                                 "<span class='uk-text-small uk-text-muted'>Father: " + v.father_name + "</span>" +
+                                "<input type='hidden' class='edit_areacode' value='" + v.areacode + "'> " +
+                                "<input type='hidden' class='edit_dssid' value='" + v.dssid + "'> " +
+                                "<input type='hidden' class='edit_mother_name' value='" + v.mother_name + "'> " +
+                                "<input type='hidden' class='edit_father_name' value='" + v.father_name + "'> " +
+                                /* "<input type='text' id='edit_hhhead' value='" + v.hhhead + "'> " +
+                                 "<input type='text' id='edit_dob' value='" + v.dob + "'> " +
+                                 "<input type='text' id='edit_study_id' value='" + v.study_id + "'> " +
+                                 "<input type='text' id='edit_gender' value='" + v.gender + "'> " +
+                                 "<input type='text' id='edit_child_name' value='" + v.child_name + "'> " +*/
                                 "</div>" +
                                 "</li>";
                         });
@@ -225,13 +291,19 @@
     }
 
     function showForm(obj) {
+        $('#dssid').val($(obj).parent('li').find('.edit_dssid').val());
+        $('#mother_name').val($(obj).parent('li').find('.edit_mother_name').val());
+        $('#father_name').val($(obj).parent('li').find('.edit_father_name').val());
+        /*$('#areacode').val($(obj).find('.edit_areacode').val());
+        $('#hhhead').val($('#edit_hhhead').val());
+        $('#dob').val($('#edit_dob').val());
+        $('#study_id').val($('#edit_study_id').val());
+        $('#gender').val($('#edit_gender').val());
+        $('#child_name').val($('#edit_child_name').val());*/
         $('#Form').css('display', 'block');
-        $('.datePicker').kendoDatePicker({
-            format: "dd-MM-yyyy"
-        });
     }
 
-    function addUsers() {
+    function addData() {
         $('#full_name').removeClass('md-input-danger');
         $('#username').removeClass('md-input-danger');
         $('#password').removeClass('md-input-danger');
